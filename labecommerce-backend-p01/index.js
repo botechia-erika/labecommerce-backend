f16c4b4f01bd0ac57d0664d5343fcdf872d1b95c
botechia-erika <mendoncabotechia@proton.me>
2023-04-12 02:51:57 -0300

    terminando exercicios aula 1

--- labecommerce-backend-p01/index.js
+++ labecommerce-backend-p01/index.js
@@ -1,1 +1,9 @@
+console.log('*************************************************************')
 console.log('start labcommerce p1')
+console.log('*************************************************************')
+const arg2 = process.argv[2]
+if(!arg2){
+    console.log('argumento nao registrado')
+}else{
+    console.log(arg2 + "🍌")  
+}
