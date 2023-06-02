0763f323af3d97efbd79efbf6a76a3af8b0fc45c
botechia-erika <mendoncabotechia@proton.me>
2023-06-02 06:52:02 -0300

    reestruturacao rama node-package-json ok

--- labecommerce-backend-p01/index.js
+++ labecommerce-backend-p01/index.js
@@ -1,9 +1,24 @@
 console.log('*************************************************************')
-console.log('start labcommerce p1')
+console.log('iniciando labcommerce -b1')
 console.log('*************************************************************')
 const arg2 = process.argv[2]
-if(!arg2){
+if (!arg2) {
     console.log('argumento nao registrado')
-}else{
-    console.log(arg2 + "🍌")  
+} else {
+    console.log(arg2 + "🍌")
 }
+
+
+import { Express } from "express";
+import { Request as req, Response as res } from '/express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("WS RODANDO NO PORT")
+})
+
+
+app.get('3333', req, res => {
+    console.log(`3333= RODANDO WS`)
+})
